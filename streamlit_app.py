c164b8c5922447f643bddad73a47f54f74a482b2
aleyeyey <aleraniaumar1@gmail.com>
2025-04-23 18:23:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("aley's space :>")
 st.write(
-    "heyyiee, welcome to my space :O."
+    "heyyiee, welcome to my space :O"
 )
 st.image("Snaptik.app_74466485541243159223.jpg")
