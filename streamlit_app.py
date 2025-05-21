a5c144bdac159a64e44c99944f82678625d1dfdf
aleyeyey <aleraniaumar1@gmail.com>
2025-05-21 14:46:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write(
     "heyyiee, welcome to my space :O"
 )
 st.image("Snaptik.app_74466485541243159223.jpg")
-st.image("1746691690002.jpg")
+st.image("1746691690002.jpg", width:200)
 st.write("\n")
 st.subheader("aley")
 st.write(
@@ -17,3 +17,10 @@ st.write(
     - cause i know that you feel me somehow
     """
 )
+st.header("check nilai ganjil genap katanya")
+angka = st.number_input("write a number:", value=0, step=1)
+
+if(angka % 2) == 0:
+    st.write(f"{angka} adalah bilangan genap")
+    else:
+    st.write(f"{angka} adalah bilangan ganjil")
