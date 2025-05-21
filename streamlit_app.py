049c277f62c3b769c75a6142d7de7cbfa3a0c2a8
aleyeyey <aleraniaumar1@gmail.com>
2025-05-21 14:34:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ st.write(
     "heyyiee, welcome to my space :O"
 )
 st.image("Snaptik.app_74466485541243159223.jpg")
+st.image("1746691690002.jpg")
+st.write("\n")
+st.subheader("aley ^^")
+st.write("lagi suka iris by goo goo dolls")
+st.write(
+    """
+    - and i'd give up forever to touch you
+    - cause i know that you feel me somehow
+    """
